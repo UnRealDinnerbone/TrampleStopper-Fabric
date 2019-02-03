98e68756d2a2409f7b60216956be93584857e35e
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2019-02-02 20:24:14 -0600

    More code cleanup

--- src/main/java/com/unrealdinnerbone/tramplestopper/TrampleStopper.java
+++ src/main/java/com/unrealdinnerbone/tramplestopper/TrampleStopper.java
@@ -11,9 +11,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-
 public class TrampleStopper implements ModInitializer {
-
 
     @Override
 	public void onInitialize() {
